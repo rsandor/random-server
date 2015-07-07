c468dd43278526815f60e323ae93dbbd415760d0
Ryan Sandor Richards <ryan@runnable.com>
2015-07-07 15:09:01 -0700

    Random integer generation.

--- random.js
+++ random.js
@@ -3,7 +3,7 @@ var http = require('http'),
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end(Math.random() + "\n");
+  response.end(parseInt(100*(Math.random())) + "\n");
 }).listen(port);
 
 console.log("Server Running on port " + port);
