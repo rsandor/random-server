ce17bc56ffddbe37a8fea01cd6e379856063b8b2
Ryan Sandor Richards <ryan@runnable.com>
2015-07-08 13:29:19 -0700

    Random cos numbers.

--- random.js
+++ random.js
@@ -3,7 +3,7 @@ var http = require('http'),
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end(Math.random() + "\n");
+  response.end(Math.cos(2*Math.PI*Math.random()) + "\n");
 }).listen(port);
 
 console.log("Server Running on port " + port);
