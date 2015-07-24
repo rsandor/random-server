1f48f9a4e460715d1a516b3fd2433b5f49406041
Ryan Sandor Richards <ryan@runnable.com>
2015-07-24 16:16:47 -0700

    Quick push.

--- random.js
+++ random.js
@@ -1,6 +1,7 @@
 var http = require('http'),
 	port = 8000;
 
+// ints branch
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end(parseInt(100*(Math.random())) + "\n");
