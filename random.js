ec5390c4ddf557601d7b076f681b1a735c6f1a13
Ryan Sandor Richards <ryan@runnable.com>
2015-09-22 15:19:44 -0700

    Quick push for update in beta

--- random.js
+++ random.js
@@ -1,10 +1,9 @@
 var http = require('http'),
 	port = 8000;
 
-// ints branch
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end(parseInt(100*(Math.random())) + "\n");
 }).listen(port);
 
-console.log("Server Running on port " + port);
+console.log("Server (Integers) Running on port " + port);
