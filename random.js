de3f0d0d31c403f793c8454aa0ff1500dcd34638
Ryan Sandor Richards <ryan@runnable.com>
2015-09-25 19:30:34 -0700

    WORDS

--- random.js
+++ random.js
@@ -1,9 +1,14 @@
 var http = require('http'),
 	port = 8000;
 
+
+var words = ['foo', 'bar', 'baz', 'bam'];
+
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end(parseInt(100*(Math.random())) + "\n");
+  var i = parseInt(words.length * Math.random());
+  var word = words[i];
+  response.end(word + "\n");
 }).listen(port);
 
 console.log("Server (Int) Running on port " + port);
